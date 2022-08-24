ebf9672ae966ffba270033ed2c0f960639f54807
Luan Braz <luan_silvacpv@hotmail.com>
2022-08-24 19:35:30 -0300

    update

---
+++ src/main/java/com/luan/ofertasregistros/OfertasregistrosApplication.java
@@ -0,0 +1,13 @@
+package com.luan.ofertasregistros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfertasregistrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfertasregistrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luan/ofertasregistros/OfertasregistrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luan.ofertasregistros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfertasregistrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
